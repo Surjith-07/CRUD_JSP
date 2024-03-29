e45165c8acf7212736020aac151c9386b9b78c0e
Surjith <106502056+Surjith-07@users.noreply.github.com>
2024-03-29 18:50:40 +0530

    Update DbConnection.java

--- App/src/main/java/studentutil/db/DbConnection.java
+++ App/src/main/java/studentutil/db/DbConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-	private static final String url = "jdbc:mysql://localhost:3306/Myconnection";
+	private static final String url = "jdbc:mysql://localhost:3306/****";
 	private static final String userName = "root";
-	private static final String password = "surjith";
+	private static final String password = "****";
 
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
